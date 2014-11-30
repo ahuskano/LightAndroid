27fe5fbd0660801c1a055d59c4d7f8506123eebb
dmacan23 <dmacan23@gmail.com>
2014-11-30 14:20:13 +0100

    - Added LightCameraActivity for native camera

---
+++ lightAndroid/src/main/java/com/dmacan/lightandroid/navigation/activity/LightCameraActivity.java
@@ -0,0 +1,11 @@
+package com.dmacan.lightandroid.navigation.activity;
+
+/**
+ * Created by dmacan on 17.11.2014..
+ */
+public abstract class LightCameraActivity extends LightActivity {
+    @Override
+    public void main() {
+
+    }
+}
