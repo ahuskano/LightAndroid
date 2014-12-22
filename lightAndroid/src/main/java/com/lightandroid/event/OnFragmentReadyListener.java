0fd29da6a245b0a770b21b45214b4876c153da8f
dmacan23 <dmacan23@gmail.com>
2014-12-22 11:06:29 +0100

    - Added OnFragmentReadyListener

---
+++ lightAndroid/src/main/java/com/lightandroid/event/OnFragmentReadyListener.java
@@ -0,0 +1,12 @@
+package com.lightandroid.event;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by David on 7.12.2014..
+ */
+public interface OnFragmentReadyListener {
+
+    public void onFragmentReady(Fragment fragment, String tag, int id);
+
+}
